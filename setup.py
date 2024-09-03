188662622ce9ae4551ab479536d6dfb6a424dfdb
Ether717 <95562180+Ether717@users.noreply.github.com>
2024-09-03 23:17:17 +0800

    v 0.2.0 Release

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="blurhash-avif",
-    version="0.1.0",
-    packages=find_packages(),
+    version="0.2.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "numpy",
         "Pillow[avif]",
