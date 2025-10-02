464a43a28b02fd444d987de7cbe56cc6706e1ba4
ZuidVolt <czuid@icloud.com>
2025-10-02 13:54:09 +0800

    Delete test_impl.py

--- src/blurhash_avif/test_impl.py
+++
@@ -1,9 +0,0 @@
-import blurhash_avif as bha
-
-if __name__ == "__main__":
-    avif_path = "/Users/cooper/Desktop/_DSC2512.avif"
-    blurhash = bha.encode(avif_path)
-    if blurhash:
-        print(f"BlurHash: {blurhash}")
-    else:
-        print("Failed to generate BlurHash")
